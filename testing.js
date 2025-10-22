a1ddbf58e7d1b5279ae5079d521ddc0c8d67f3d0
Wole Akinyemi <you@example.com>
2025-10-21 20:33:41 -0500

    adding my vscode to the repo in github

---
+++ testing.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Alice",
+    age: 30,
+    city: "New York"
+    greet : function() {
+        alert("Hello my name is wole " + this.name);}
+
+    };
+
+    person.greet(Can i see how this works);
